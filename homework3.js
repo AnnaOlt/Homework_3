1bd4a2bad7cc04bc2848df2d6c7d0999e2fff763
Anna Olt <anna.olt16@gmail.com>
2020-01-17 20:07:07 -0800

    linked button events to js functions

--- homework3.js
+++ homework3.js
@@ -1,4 +1,85 @@
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0987654321";
-var specialCharacter = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+
+// // variables not in HTML used to create random passowrd
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = [0, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+// var specialCharacter = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
+
+// // variables from HTML
+// var password = document.querySelector("#randomPassword");
+// var btnOne = document.querySelector("#btn1");
+// var btnTwo = document.querySelector("#btn2");
+
+// function generateRandom() {
+//   document.getElementById("btn1").innerHTML = math.random();
+//   console.log(math.random);
+// }
+
+// function copyRandom() {
+//   console.log("copy to clipboard");
+// }
+
+function generateRandom() {
+  var numbers = Math.floor(Math.random() * 100 + 1);
+  document.getElementById("randomPassword").innerHTML = numbers;
+}
+
+function copyRandom() {
+  var lowerCase = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z"
+    ],
+    valToUse = lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  console.log(valToUse);
+}
